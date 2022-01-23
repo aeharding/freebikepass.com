68838150737108e5fb0693018b4ac361bd61fad4
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2022-01-23 17:45:58 -0600

    Add bike silhouette, misc tweaks

--- src/routes/home/Section5.tsx
+++ src/routes/home/Section5.tsx
@@ -1,18 +1,40 @@
 import styled from "@emotion/styled/macro";
-import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight, faCar, faTags } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Section from "../../shared/Section";
+import { ReactComponent as BikeFamily } from "./bikeFamily.svg";
 
 const StyledSection5 = styled.div`
+  position: relative;
+
   display: flex;
   padding: 0 0 2rem;
+  overflow: hidden;
 
   background: linear-gradient(to right, #a5fecb, #20bdff, #5433ff);
   color: black;
 `;
 
+const StyledBikeFamily = styled(BikeFamily)`
+  position: absolute;
+
+  bottom: 0;
+  right: 0;
+
+  width: 60%;
+  max-width: 600px;
+
+  @media (max-width: 800px) {
+    width: 100%;
+  }
+
+  transform: rotate(0.5deg) translateY(1%);
+  opacity: 0.2;
+`;
+
 const Title = styled.h3`
   font-size: 3rem;
+  margin: 2rem 0;
 
   font-weight: 100;
 
@@ -35,7 +57,9 @@ const Title = styled.h3`
 const Benefits = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  grid-gap: 1rem;
+  grid-gap: 2rem;
+
+  position: relative;
 `;
 
 const Benefit = styled.a`
@@ -46,14 +70,18 @@ const Benefit = styled.a`
   padding: 1rem;
 
   border: 1px solid rgba(255, 255, 255, 0.3);
-  background: rgba(255, 255, 255, 0.3);
+  background: rgba(255, 255, 255, 0.4);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   border-radius: 1rem;
   text-decoration: none;
+  backdrop-filter: blur(4px);
 
   transition: transform 100ms ease-out;
 
-  &:hover {
-    transform: scale(1.02);
+  @media (hover: hover) {
+    &:hover {
+      transform: scale(1.02);
+    }
   }
 `;
 
@@ -61,6 +89,15 @@ const BenefitTitle = styled.h4`
   font-size: 1.1rem;
   font-weight: normal;
   margin: 0;
+
+  display: flex;
+  align-items: center;
+
+  svg {
+    font-size: 1.3em;
+    margin-right: 1rem;
+    margin-left: 0.5rem;
+  }
 `;
 
 const BenefitDescription = styled.p`
@@ -69,16 +106,25 @@ const BenefitDescription = styled.p`
   font-size: 0.9rem;
 `;
 
-const BenefitGo = styled(FontAwesomeIcon)`
-  font-size: 2rem;
-  color: rgba(255, 255, 255, 0.5);
+const BenefitGo = styled.div`
+  align-self: flex-end;
 
-  align-self: end;
+  display: flex;
+  align-items: center;
+  grid-gap: 1rem;
+
+  color: white;
+
+  svg {
+    color: rgba(255, 255, 255, 0.5);
+    font-size: 2rem;
+  }
 `;
 
 export default function Section5() {
   return (
     <StyledSection5>
+      <StyledBikeFamily />
       <Section>
         <Title>
           There's <strong>more</strong> <aside>benefits to #GoByBike</aside>
@@ -90,28 +136,37 @@ export default function Section5() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <BenefitTitle>Emergency Ride Home program</BenefitTitle>
+            <BenefitTitle>
+              <FontAwesomeIcon icon={faCar} /> Emergency Ride Home
+            </BenefitTitle>
             <BenefitDescription>
               Do you worry about if your bike breaks down during your ride, or
               the weather gets bad while you're out?{" "}
-              <strong>Get a free lift!</strong> Participants are eligible for up
-              to six Union Cab vouchers per year, good for up to $75 per ride.
+              <strong>Get a free lift!</strong> You can redeem six Union Cab
+              vouchers per year, good for up to $75 per ride.
             </BenefitDescription>
 
-            <BenefitGo icon={faAngleRight} />
+            <BenefitGo>
+              <span>I'm Down</span> <FontAwesomeIcon icon={faAngleRight} />
+            </BenefitGo>
           </Benefit>
           <Benefit
             href="https://bicyclebenefits.org/cities/6"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <BenefitTitle>The Bicycle Benefits program</BenefitTitle>
+            <BenefitTitle>
+              <FontAwesomeIcon icon={faTags} /> Bicycle Benefits Program
+            </BenefitTitle>
 
             <BenefitDescription>
-              Get discounts for biking to many local Madison retailers -
-              including Fresh Madison, Metcalfe's, Ian's Pizza, and more!
+              Get discounts for riding to many local Madison shops - including
+              Fresh Madison, Willy Street Co-op, Metcalfe's, Ian's Pizza, and
+              more!
             </BenefitDescription>
-            <BenefitGo icon={faAngleRight} />
+            <BenefitGo>
+              <span>Let's Do This</span> <FontAwesomeIcon icon={faAngleRight} />
+            </BenefitGo>
           </Benefit>
         </Benefits>
       </Section>
